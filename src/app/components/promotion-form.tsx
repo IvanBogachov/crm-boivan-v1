f370e94127f700b3451527316e6e365553cf21e8
IvanBogachov <106016809+IvanBogachov@users.noreply.github.com>
2025-02-25 21:50:51 +0200

    add ....

--- src/app/components/promotion-form.tsx
+++ src/app/components/promotion-form.tsx
@@ -74,12 +74,19 @@ export default function PromotionForm({
       <Form className="flex flex-col gap-10">
         <p className="mb-0.5 text-xl">Add new promotion</p>
         <div className="flex flex-col gap-5">
-          <InputField required label="Title" placeholder="Title" name="title" />
+          <InputField
+            required
+            label="Title"
+            placeholder="Title"
+            name="title"
+            as="input" // Додано as="input"
+          />
           <InputField
             required
             label="Description"
             placeholder="Description"
             name="description"
+            as="input" // Додано as="input"
           />
           <InputField
             required
@@ -87,6 +94,7 @@ export default function PromotionForm({
             label="Discount"
             placeholder="Discount"
             name="discount"
+            as="input" // Додано as="input"
           />
           <LogoUploader square label="Image" placeholder="Upload photo" />
         </div>
